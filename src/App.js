ba1784a959a2169193647d007c8ae3cb35e802fa
Samarth Dadhaniya <samarthdadhaniya@Samarths-MacBook-Air.local>
2024-10-17 22:33:48 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Chandan Tiwari!
+          Hello Samarth Dadhaniya
         </a>
       </header>
     </div>
